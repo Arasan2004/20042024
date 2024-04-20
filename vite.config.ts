bcc050ae63e3738bc383f267dd067ab03b40f534
Arasan2004 <167637275+Arasan2004@users.noreply.github.com>
2024-04-20 22:31:55 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/will-you-be-my-valentine",
+  base: "/Will you Come With Me On 25/4??? 🥹",
   plugins: [react()],
 });
